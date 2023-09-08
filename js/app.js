821702ed081f49f03c5b5e0f5c2b4d9f54ad2629
Eduardo Quiroga <quirogagis@gmail.com>
2023-09-08 16:07:17 -0300

    Funcionalidad al slider con javascript

---
+++ js/app.js
@@ -0,0 +1,55 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlContainer = document.querySelector('.gallery-controls'); // Corrección aquí
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+    constructor(container, item, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls; // Cambio de nombre de variable
+        this.carouselArray = [...item];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
+        });
+    }
+
+    setCurrentState(direction) {
+        if (direction.classList.contains('gallery-controls-previous')) {
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else {
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            galleryControlContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+            document.querySelector(`.gallery-controls-${control}`).innerText = control;
+        });
+    }
+
+    useControls() {
+        const triggers = [...galleryControlContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls); 
+exampleCarousel.setControls();
+exampleCarousel.useControls();
